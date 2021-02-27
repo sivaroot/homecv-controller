0a27a1603d84cd06590708477c3c5c081a84332f
sivaroot <sivaroot.ohm@odds.team>
2021-02-27 19:53:49 +0700

    [ADD] network info class interface

---
+++ net/interface/network_info.py
@@ -0,0 +1,12 @@
+class NetworkInfo:
+    def __init__(self, ip, mac):
+        self.ip = ip
+        self.mac = mac
+
+    def get_ip_addr(self):
+        return self.ip
+
+    def get_mac_addr(self):
+        return self.mac
+    def to_string(self):
+        return self.ip + " " + self.mac
